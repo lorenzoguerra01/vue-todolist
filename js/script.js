f9ebcc002c278fc0ec9851b144bdfec2f13145c9
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-02 17:13:12 +0200

    add removeItem

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ createApp({
             });
             item.done = !item.done;
         },
+        removeItem(id) {
+            const i = this.toDo.findIndex((el) => el.id === id)
+            if (i !== -1) {
+                this.toDo.splice(i, 1);
+            }
+        },
     },
     mounted() {
 
